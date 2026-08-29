d88706cb4e1b9f2bc46835ac939cd57277cf2754
BobTheBuilder139 <bobhateaarush@gmail.com>
2026-08-29 21:08:40 +0530

    Masks and tensor prep
    Added masks for detecting all types of information in SAR datasets(ships,land,oil-spills)
    converted images into tensors to use for UNet(image classification)

--- oil_spill_dataset.py
+++ oil_spill_dataset.py
@@ -1,3 +1,9 @@
+# Cyan   → 0 → Oil spill
+# Red    → 1 → Look-alike
+# Brown  → 2 → Ship
+# Green  → 3 → Land
+# Black  → 4 → Sea
+#dhluv pls review
 import cv2 as cv
 import os
 import numpy as np
@@ -9,7 +15,7 @@ class OilSpillData(Dataset):
         self.image_dir = image_dir
         self.label_dir = label_dir
 
-        extensions = ('.jpg', '.png', '.jpeg')
+        extensions = ('.jpg', '.png', '.jpeg', '.tif')
         self.images = [f for f in os.listdir(image_dir) if f.lower().endswith(extensions)]
         self.images.sort()
 
@@ -41,5 +47,22 @@ class OilSpillData(Dataset):
         label = cv.cvtColor(label,cv.COLOR_BGR2RGB)
         label = cv.resize(label,(512,256), interpolation=cv.INTER_NEAREST)
 
-        oil_mask = np.zeros((256,512),dtype=np.int64) #creating mask for each class
-        black = np.all(label == [0,0,0],axis = -1) # evaluates every pixel in the image to check if it is black,for each individual pixel returns false if even one of the rgb values in the channel is non zero
+        mask = np.zeros((256,512),dtype=np.int64) #creating mask for each class
+        oil = np.all(label == [0,255,255],axis = -1) # evaluates every pixel in the image to check if it is cyan,for each individual pixel returns false if even one of the rgb values in the channel is non zero
+        look_alike = np.all(label == [255,0, 0], axis = -1)#red
+        ship = np.all(label == [153,76,0], axis = -1)
+        land = np.all(label == [0,153,0],axis = -1)
+        sea = np.all(label == [0,0,0], axis = -1)
+
+        #creating class ids
+        mask[oil] = 0
+        mask[look_alike] = 1
+        mask[ship] = 2
+        mask[land] = 3
+        mask[sea] = 4
+
+        image = torch.tensor(image, dtype = torch.float32).unsqueeze(0) #converting image into a tensor for UNet model for processing and adding a batch dimension
+        mask = torch.tensor(mask, dtype = torch.long)
+
+        return image,mask
+
